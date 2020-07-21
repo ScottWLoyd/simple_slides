a6b36a876e4c153e7611f39fe66a6e35c81b3f1f
Loyd, Scott <Scott.Loyd@gtri.gatech.edu>
2020-07-21 16:31:01 -0400

    added premake binary

--- build.sh
+++
@@ -1,6 +0,0 @@
-mkdir build
-pushd build
-
-gcc ../src/main.c -I../src -I/usr/include/SDL2 -o simple_slides `sdl2-config --cflags --static-libs` -lSDL2_ttf
-
-popd
---
+++ premake5.lua
@@ -0,0 +1,27 @@
+workspace "SimpleSlides"
+    configurations { "Debug", "Release" }
+
+project "SimpleSlides"
+    kind "ConsoleApp"
+    language "C"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files 
+    {
+        "src/**.h",
+        "src/**.c"
+    }
+
+    filter "configurations:Debug"
+        defines
+        {
+            "DEBUG"
+        }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines
+        {
+            "NDEBUG"
+        }
+        optimize "On"
